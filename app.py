8626847420c523120479d1914d236139a756838b
mohamedx2 <hamroun1969@gmail.com>
2024-11-03 17:16:31 +0100

    first commit

--- app.py
+++ app.py
@@ -100,5 +100,6 @@ def compile_text():
     output = compile_and_run_code(program_lines)
     return jsonify({"output": output})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
